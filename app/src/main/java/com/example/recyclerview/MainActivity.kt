25f0e0f0aa6ee42428519adf7aeacd567cd3ec82
unknown <serhiiromanchuk@gmail.com>
2023-05-07 17:43:44 +0300

    Implement UserListFragment

--- app/src/main/java/com/example/recyclerview/MainActivity.kt
+++ app/src/main/java/com/example/recyclerview/MainActivity.kt
@@ -12,55 +12,5 @@ import com.example.recyclerview.model.adapter.UserActionListener
 import com.example.recyclerview.model.adapter.UserListAdapter
 
 class MainActivity : AppCompatActivity() {
-    private val binding: ActivityMainBinding by lazy {
-        ActivityMainBinding.inflate(layoutInflater)
-    }
-    private val userListAdapter: UserListAdapter by lazy {
-        UserListAdapter(object : UserActionListener {
-            override fun onUserMove(user: User, moveBy: Int) {
-                usersService.moveUser(user, moveBy)
-            }
 
-            override fun onUserDelete(user: User) {
-                usersService.deleteUser(user)
-            }
-
-            override fun onUserDetails(user: User) {
-                Toast.makeText(
-                    this@MainActivity,
-                    "${user.name} - ${user.company}",
-                    Toast.LENGTH_SHORT
-                ).show()
-            }
-
-            override fun onUserFire(user: User) {
-                usersService.fireUser(user)
-            }
-        })
-    }
-
-    private val usersService: UsersService
-        get() = (applicationContext as App).usersService
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(binding.root)
-
-        usersService.addListener(usersListener)
-        binding.recyclerView.adapter = userListAdapter
-        val itemAnimator = binding.recyclerView.itemAnimator
-        if (itemAnimator is DefaultItemAnimator) {
-            itemAnimator.supportsChangeAnimations = false
-        }
-
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        usersService.removeListener(usersListener)
-    }
-
-    private val usersListener: UserListener = {
-        userListAdapter.submitList(it)
-    }
 }
--- app/src/main/java/com/example/recyclerview/screens/UserListFragment.kt
+++ app/src/main/java/com/example/recyclerview/screens/UserListFragment.kt
@@ -5,15 +5,79 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.recyclerview.widget.DefaultItemAnimator
+import com.example.recyclerview.App
 import com.example.recyclerview.R
+import com.example.recyclerview.databinding.ActivityMainBinding
+import com.example.recyclerview.databinding.FragmentUserListBinding
+import com.example.recyclerview.model.User
+import com.example.recyclerview.model.UserListener
+import com.example.recyclerview.model.UsersService
+import com.example.recyclerview.model.adapter.UserActionListener
+import com.example.recyclerview.model.adapter.UserListAdapter
 
 class UserListFragment : Fragment() {
+
+    private var _binding: FragmentUserListBinding? = null
+    private val binding: FragmentUserListBinding
+        get() = _binding ?: throw RuntimeException("FragmentUserListBinding == null")
+
+    private val userListAdapter: UserListAdapter by lazy {
+        UserListAdapter(object : UserActionListener {
+            override fun onUserMove(user: User, moveBy: Int) {
+                usersService.moveUser(user, moveBy)
+            }
+
+            override fun onUserDelete(user: User) {
+                usersService.deleteUser(user)
+            }
+
+            override fun onUserDetails(user: User) {
+                Toast.makeText(
+                    requireContext(),
+                    "${user.name} - ${user.company}",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+
+            override fun onUserFire(user: User) {
+                usersService.fireUser(user)
+            }
+        })
+    }
+
+    private val usersService: UsersService
+        get() = (requireContext().applicationContext as App).usersService
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_user_list, container, false)
+        _binding = FragmentUserListBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        usersService.addListener(usersListener)
+        binding.recyclerView.adapter = userListAdapter
+        val itemAnimator = binding.recyclerView.itemAnimator
+        if (itemAnimator is DefaultItemAnimator) {
+            itemAnimator.supportsChangeAnimations = false
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        usersService.removeListener(usersListener)
+        _binding = null
+    }
+
+    private val usersListener: UserListener = {
+        userListAdapter.submitList(it)
     }
 }
